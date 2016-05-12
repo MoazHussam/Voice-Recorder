ff7b549b490b400924b3b23eef10ddb932b7a8e3
Moaz Ahmed <moaz_saad@hotmail.com>
2016-05-12 02:24:32 +0200

    Initial Commit

---
+++ Voice Recorder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Voice Recorder
+//
+//  Created by Moaz Ahmed on 5/12/16.
+//  Copyright © 2016 Moaz Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
